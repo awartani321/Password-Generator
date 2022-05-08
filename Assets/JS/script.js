6bf7caa5849f2878ded573515acdbd6601cb27cc
Adam Awartani <adamawartani321@gmail.com>
2022-05-08 15:32:21 -0400

    added prompts for numbers, special characters, upper and lowercase characters

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,7 @@
 var characterLength = 8;
 var choiceArray =[];
 
-var specialCharacters =['!','@','#','$','%','^','&','*','(',')','-','_','<','>','/']
+var specialCharactersArray =['!','@','#','$','%','^','&','*','(',')','-','_','<','>','/']
 var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
 var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
 var numberArray = ['1','2','3','4','5','6','7','8','9','0',]
@@ -25,6 +25,26 @@ function generatePassword() {
     // I would generatePassword based on the prompts
 }
 function getPrompts() {
-    charLength = parseInt(prompt(""
-}
+    charLength = parseInt(prompt("How many characters would you like your password to be? (8-128 characters"));   
+    
+    //Not A Number(NaN)
 
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) { //This should be false
+    alert("Character length must be a number of 8-128 digits. Try Again");
+    return false;
+    }
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+        choiceArray = choiceArray.concat(lowerCaseArray);
+    }
+    if (confirm("Would you like uppercase letters in your password?")) {
+        choiceArray = choiceArray.concat(upperCaseArray);
+    }
+    if (confirm("Would you like special characters in your password?")) {
+        choiceArray = choiceArray.concat(specialCharactersArray);
+}
+    if (confirm("Would you like numbers in your password?")) {
+    choiceArray = choiceArray.concat(numberArray);
+}
+return true;
+}
